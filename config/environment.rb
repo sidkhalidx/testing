22094e06d016ab5c0325c690959ba46469f2edb5
sidkhalidx <siddique.khalid@xprolabs.com>
2021-11-23 18:43:08 +0500

    a

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,6 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   authentication: :plain,
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
+  ssl: true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'simple-railss.herokuapp.com' }
 end
